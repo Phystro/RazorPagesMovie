2a9938c6347a74526005beefd03071e55949a5b3
Phystro <phystroq@gmail.com>
2023-03-25 02:19:16 +0300

    add: data searching and flitering

--- Pages/Movies/Index.cshtml.cs
+++ Pages/Movies/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Data;
 using RazorPagesMovie.Models;
@@ -19,14 +20,45 @@ namespace RazorPagesMovie.Pages.Movies
             _context = context;
         }
 
-        public IList<Movie> Movie { get;set; } = default!;
+        public IList<Movie> Movie { get; set; } = default!;
+
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+
+        public SelectList? Genres { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? MovieGenre { get; set; }
+
+        // public async Task OnGetAsync()
+        // {
+        //     if (_context.Movies != null)
+        //     {
+        //         Movie = await _context.Movies.ToListAsync();
+        //     }
+        // }
 
         public async Task OnGetAsync()
-        {
-            if (_context.Movies != null)
-            {
-                Movie = await _context.Movies.ToListAsync();
-            }
-        }
+{
+    // Use LINQ to get list of genres.
+    IQueryable<string> genreQuery = from m in _context.Movies
+                                    orderby m.Genre
+                                    select m.Genre;
+
+    var movies = from m in _context.Movies
+                 select m;
+
+    if (!string.IsNullOrEmpty(SearchString))
+    {
+        movies = movies.Where(s => s.Title!.Contains(SearchString));
+    }
+
+    if (!string.IsNullOrEmpty(MovieGenre))
+    {
+        movies = movies.Where(x => x.Genre == MovieGenre);
+    }
+    Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+    Movie = await movies.ToListAsync();
+}
     }
 }
